f567038a6af1710290e940c7e59796310dd80c4e
baibhav <baibhavtiwari37@gmail.com>
2022-11-19 01:05:33 +0530

    logo

--- blockify/data/activities.js
+++ blockify/data/activities.js
@@ -4,7 +4,7 @@ export const activities = [
     title: 'Ambient Gold',
     subTitle: 'Lo-Fi',
     avatar:
-      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+      'https://avatars.dicebear.com/api/open-peeps/deepanshu.svg',
   },
   {
     index: 2,
